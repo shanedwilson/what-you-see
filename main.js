249dc26a1eb16395e7b9e889520d8692a2ae40dc
Shane D Wilson <me@shanedwilson.com>
2018-09-24 21:02:08 -0500

    2 people

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const famous = [
+    {
+        title: "Samurai",
+        name: "Tomoe Gozen",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        lifespan: {
+          birth: 1747,
+          death: 1797
+        }
+      },
+
+      {
+        title: "Cult Leader",
+        name: "Jim Jones",
+        bio: "James Warren Jones was an American religious cult leader who initiated and was responsible for a mass suicide and mass murder in Jonestown, Guyana.",
+        image: "https://www.biography.com/.image/t_share/MTIwNjA4NjM0MDc0MjY5MTk2/jim-jones-10367607-1-402.jpg",
+        lifespan: {
+          birth: 1931,
+          death: 1978
+        }
+      }
+]
